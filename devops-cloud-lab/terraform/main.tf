e327fe65a353c56780101744d19695f7799a00a2
joshidevesh7300 <joshidevesh7300@gmail.com>
2026-01-06 15:28:02 +0530

    Add Terraform configuration for AWS instance

---
+++ devops-cloud-lab/terraform/main.tf
@@ -0,0 +1,14 @@
+provider "aws" {
+  region = "ap-south-1"
+}
+
+resource "aws_instance" "lab_vm" {
+  ami           = "ami-0f5ee92e2d63afc18"
+  instance_type = "t3.micro"
+  key_name      = "lab-key"
+
+  tags = {
+    Name = "devops-lab-vm"
+    Owner = "student"
+  }
+}
