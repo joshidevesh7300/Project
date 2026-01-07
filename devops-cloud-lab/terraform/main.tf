a761f59b887a3e16ee8bc8dd05906a8c4ddba0d9
joshidevesh7300 <joshidevesh7300@gmail.com>
2026-01-07 10:07:31 +0530

    Change key_name for AWS instance to 'cloud'

--- devops-cloud-lab/terraform/main.tf
+++ devops-cloud-lab/terraform/main.tf
@@ -5,7 +5,7 @@ provider "aws" {
 resource "aws_instance" "lab_vm" {
   ami           = "ami-0f5ee92e2d63afc18"
   instance_type = "t3.micro"
-  key_name      = "lab-key"
+  key_name      = "cloud"
 
   tags = {
     Name      = "devops-lab-vm"
