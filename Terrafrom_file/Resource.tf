8181682c836ccb6dbb093188f3b1f77ce5f5609a
joshidevesh7300 <joshidevesh7300@gmail.com>
2026-03-12 09:45:10 +0530

    Define AWS resources in Resource.tf
    Added resources for AWS instance, VPC, and subnet with tags.

--- Terrafrom_file/Resource.tf
+++ Terrafrom_file/Resource.tf
@@ -1,9 +1,33 @@
-provider "aws"{
-  region ="ap-south-1"
+provider "aws" {
+  region = "ap-south-1"
+}
+
+resource "aws_instance" "lab-id1" {
+
+  ami           = "ami-0f5ee92e2d63afc18"
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "lab-id1"
   }
 
-resource "aws_instance" "lab-id1"{
-  tag{
-    Name = "lab-id1"
-    }
+}
+
+resource "aws_vpc" "lab-id1-v1" {
+  cidr_block = "10.0.0.0/16"
+
+  tags = {
+    Name = "lab-id1-v1"
+  }
+}
+
+resource "aws_subnet" "lab-id1-s1" {
+
+  vpc_id            = aws_vpc.lab-id1-v1.id
+  cidr_block        = "10.0.1.0/24"
+  availability_zone = "ap-south-1a"
+
+  tags = {
+    Name = "lab-id1-s1"
+  }
 }
